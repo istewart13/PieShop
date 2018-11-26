f32a5fa1c43cf2e673121f8782caa39854d2bd9b
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-11-26 15:30:56 +0000

    Add Mock Pie and mock Category repos

---
+++ BethanysPieShop/Models/MockPieRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class MockPieRepository : IPieRepository
+    {
+        private readonly ICategoryRepository _categoryRepository = new MockCategoryRepository();
+
+        public IEnumerable<Pie> Pies
+        {
+            get
+            {
+                return new List<Pie>
+                {
+                    new Pie { PieId = 1, Name = "Strawberry Pie", Price = 15.95M, ShortDescription = "" },
+                    new Pie { PieId = 2, Name = "Cheese cake", Price = 18.95M, ShortDescription = "" },
+                    new Pie { PieId = 3, Name = "Rhubarb Pie", Price = 15.95M, ShortDescription = "" },
+                    new Pie { PieId = 4, Name = "Pumpkin Pie", Price = 12.95M, ShortDescription = "" }
+                };
+            }
+        }
+
+        public IEnumerable<Pie> PiesOfTheWeek => throw new NotImplementedException();
+
+        public Pie GetPieById(int pieId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
